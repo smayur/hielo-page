f601b1e43e47fc86cfe8c02186386dd291da0b8c
mayurS <mayurs@prdxn.com>
2020-10-25 19:28:37 +0530

    add script for burger

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,6 @@
-/* Author: 
+var burger = document.querySelector('.burger');
 
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+burger.addEventListener('click', function(){ 
+  //Toggle nav
+  burger.classList.toggle('navtoggle');
+});
